473ad77396a9630ef00752d52067abd2a0f4a2f9
lucasliet <lucasouliveira@gmail.com>
2020-05-12 16:57:55 -0300

    Updates

---
+++ precache-manifest.43ff1096810607ef11ac526f2ab7166e.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "149f91d83777f233ccc182dfc23f61a1",
+    "url": "/Coronavirus-Dashboard/index.html"
+  },
+  {
+    "revision": "8c8ba1c7d02cf1dedde4",
+    "url": "/Coronavirus-Dashboard/static/css/2.ecd5ced7.chunk.css"
+  },
+  {
+    "revision": "f9f8691d21d0701e8c1f",
+    "url": "/Coronavirus-Dashboard/static/css/main.d4cfa3c4.chunk.css"
+  },
+  {
+    "revision": "8c8ba1c7d02cf1dedde4",
+    "url": "/Coronavirus-Dashboard/static/js/2.9ffc6f25.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Coronavirus-Dashboard/static/js/2.9ffc6f25.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9f8691d21d0701e8c1f",
+    "url": "/Coronavirus-Dashboard/static/js/main.078dc837.chunk.js"
+  },
+  {
+    "revision": "1ba2d28888f1926e377b",
+    "url": "/Coronavirus-Dashboard/static/js/runtime-main.2323f2b5.js"
+  },
+  {
+    "revision": "0b4ac1dc75df35e169b70d7719afe4cc",
+    "url": "/Coronavirus-Dashboard/static/media/notification.0b4ac1dc.ttf"
+  },
+  {
+    "revision": "5bee74caefdf9d0a834915f6c8eeb259",
+    "url": "/Coronavirus-Dashboard/static/media/notification.5bee74ca.svg"
+  },
+  {
+    "revision": "651771e1df95c807c99608188d0a4287",
+    "url": "/Coronavirus-Dashboard/static/media/notification.651771e1.woff"
+  },
+  {
+    "revision": "c0d3c94cd6112550c51d7d1ed13b9da1",
+    "url": "/Coronavirus-Dashboard/static/media/notification.c0d3c94c.eot"
+  }
+]);
