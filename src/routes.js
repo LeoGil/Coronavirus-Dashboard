6573e9f99a61fb8f65e53abfc93a8bf64d0a73aa
Leonardo Gil <leogilc@gmail.com>
2020-06-07 12:56:22 -0300

    Update routes.js
    Correção da rota # @lucasliet

--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,16 @@
 import React from 'react'
-// import { BrowserRouter, Switch, Route } from 'react-router-dom'
-import { HashRouter, Switch, Route } from 'react-router-dom'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
 import Main from './pages/main'
 import Product from './pages/product'
 
 const Routes = () => (
-    <HashRouter>
+    <BrowserRouter>
         <Switch>
             <Route exact path="/Coronavirus-Dashboard" component={Main} />
             <Route path="/Coronavirus-Dashboard/products/:id" component={Product} />
         </Switch>
-    </HashRouter>
+    </BrowserRouter>
 )
 
 export default Routes
