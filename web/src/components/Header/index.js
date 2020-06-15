2737ec393d61788ad1b63be045c535e8f069027d
Leonardo Gil <leonardo.lgc@policiacientifica.sp.gov.br>
2020-06-15 04:09:01 -0300

    blink do ao vivo

--- web/src/components/Header/index.js
+++ web/src/components/Header/index.js
@@ -5,7 +5,12 @@ import './styles.css'
 const Header = () => (
     // <header id="main-header">DASHBOARD</header>
     <div id="main-header" class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 shadow-sm">
-        <h5 class="my-0 mr-md-auto font-weight-normal">DASHBOARD</h5>
+        <h5 class="my-0 mr-md-auto font-weight-normal ">
+            <div class="blobs-container">
+                <span>DASHBOARD</span>
+                <div class="blob red"></div>
+            </div>
+        </h5>
         <nav class="my-2 my-md-0 mr-md-3">
             <a class="p-2 text-white" href="#">Features</a>
         </nav>
