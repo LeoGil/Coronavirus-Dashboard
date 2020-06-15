2b6f2403a19e2431f938853085590785d5c19595
Lucas Souza <lucasouliveira@gmail.com>
2020-06-15 08:46:43 -0300

    using gh-pages to deploy in heroku + welcome msg

--- api/src/routes.ts
+++ api/src/routes.ts
@@ -10,6 +10,14 @@ const contryController = new ContryController()
 const countriesController = new CountriesController()
 const globalController = new GlobalController()
 
+//Mensagem de boas vindas
+routes.get('/', (request, response) => {
+  return response.json({
+    message: 'Coronavirus-Dashboard Server API: https://leogil.github.io/Coronavirus-Dashboard'
+
+  })
+})
+
 //Rotas do CONTRY
 routes.get('/country/:id', contryController.show)
 
