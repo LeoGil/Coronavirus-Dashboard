bb64e6a1d6aca2965ef2dc94f5bdf6e9bd48216c
Lucas Souza <lucasouliveira@gmail.com>
2020-06-16 00:12:59 -0300

    Updates

---
+++ precache-manifest.6ef2600573feea4a569328ffe92d292c.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52b57fdaa7a66bdc1a0f5cd04b825947",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a223644d188d86aa1cc9",
+    "url": "/static/css/2.441eb6f0.chunk.css"
+  },
+  {
+    "revision": "58be74ce96b907296f0b",
+    "url": "/static/css/main.67ccb113.chunk.css"
+  },
+  {
+    "revision": "a223644d188d86aa1cc9",
+    "url": "/static/js/2.fd21dd5c.chunk.js"
+  },
+  {
+    "revision": "8bb757413c38ce564bf5d6ba97eb4669",
+    "url": "/static/js/2.fd21dd5c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58be74ce96b907296f0b",
+    "url": "/static/js/main.a609d1bb.chunk.js"
+  },
+  {
+    "revision": "bc92f2a3074fad6afbd0",
+    "url": "/static/js/runtime-main.be85a13a.js"
+  },
+  {
+    "revision": "0b4ac1dc75df35e169b70d7719afe4cc",
+    "url": "/static/media/notification.0b4ac1dc.ttf"
+  },
+  {
+    "revision": "5bee74caefdf9d0a834915f6c8eeb259",
+    "url": "/static/media/notification.5bee74ca.svg"
+  },
+  {
+    "revision": "651771e1df95c807c99608188d0a4287",
+    "url": "/static/media/notification.651771e1.woff"
+  },
+  {
+    "revision": "c0d3c94cd6112550c51d7d1ed13b9da1",
+    "url": "/static/media/notification.c0d3c94c.eot"
+  }
+]);
