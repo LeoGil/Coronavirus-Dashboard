9d4ed106fbfaa98e36ba5318ec1a9f184d4066e2
Lucas Souza <lucasouliveira@gmail.com>
2020-06-16 00:14:31 -0300

    Updates

---
+++ precache-manifest.af4021db638573ce9dc4e4fe00aa36b5.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54a89c6d1a532dd237ceb8087742ef8d",
+    "url": "/Coronavirus-Dashboard/index.html"
+  },
+  {
+    "revision": "4a9d5d7473943e8724f5",
+    "url": "/Coronavirus-Dashboard/static/css/2.32004adb.chunk.css"
+  },
+  {
+    "revision": "58be74ce96b907296f0b",
+    "url": "/Coronavirus-Dashboard/static/css/main.67ccb113.chunk.css"
+  },
+  {
+    "revision": "4a9d5d7473943e8724f5",
+    "url": "/Coronavirus-Dashboard/static/js/2.fd21dd5c.chunk.js"
+  },
+  {
+    "revision": "8bb757413c38ce564bf5d6ba97eb4669",
+    "url": "/Coronavirus-Dashboard/static/js/2.fd21dd5c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58be74ce96b907296f0b",
+    "url": "/Coronavirus-Dashboard/static/js/main.a609d1bb.chunk.js"
+  },
+  {
+    "revision": "c71146338746c5244f67",
+    "url": "/Coronavirus-Dashboard/static/js/runtime-main.82839203.js"
+  },
+  {
+    "revision": "0b4ac1dc75df35e169b70d7719afe4cc",
+    "url": "/Coronavirus-Dashboard/static/media/notification.0b4ac1dc.ttf"
+  },
+  {
+    "revision": "5bee74caefdf9d0a834915f6c8eeb259",
+    "url": "/Coronavirus-Dashboard/static/media/notification.5bee74ca.svg"
+  },
+  {
+    "revision": "651771e1df95c807c99608188d0a4287",
+    "url": "/Coronavirus-Dashboard/static/media/notification.651771e1.woff"
+  },
+  {
+    "revision": "c0d3c94cd6112550c51d7d1ed13b9da1",
+    "url": "/Coronavirus-Dashboard/static/media/notification.c0d3c94c.eot"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6ef2600573feea4a569328ffe92d292c.js"
+  "/Coronavirus-Dashboard/precache-manifest.af4021db638573ce9dc4e4fe00aa36b5.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Coronavirus-Dashboard/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
