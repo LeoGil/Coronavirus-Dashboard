980ea8fb06fbe3da50b1c05cde3c38ce6e1cbb6a
Leonardo Gil <leonardo.lgc@policiacientifica.sp.gov.br>
2020-06-19 15:46:22 -0300

    retirando comentario

--- api/src/controllers/ContryController.ts
+++ api/src/controllers/ContryController.ts
@@ -2,21 +2,6 @@ import { Request, Response } from 'express'
 import { api, api_timeline_global } from '../services/api';
 
 class ContryController {
-    // async show(request: Request, response: Response) {
-    //     const { id } = request.params
-
-    //     const responseData = await api.get(`/free-api?countryTotal=${id}`)
-
-    //     const { countrydata } = responseData.data;
-    //     const { info } = countrydata[0];
-    //     const country = {
-    //         ...info,
-    //         ...countrydata[0]
-    //     }
-    //     delete country.info
-
-    //     return response.json(country);
-    // }
 
     async show(request: Request, response: Response) {
         const { id } = request.params
@@ -33,17 +18,6 @@ class ContryController {
 
         return response.json(country);
     }
-
-    // async timeline(request: Request, response: Response) {
-    //     const { id } = request.params
-
-    //     const responseData = await api.get(`/free-api?countryTimeline=${id}`)
-
-    //     const { timelineitems } = responseData.data;
-    //     delete timelineitems[0]['stat']
-
-    //     return response.json(timelineitems);
-    // }
 
     async timeline(request: Request, response: Response) {
         const { id } = request.params
