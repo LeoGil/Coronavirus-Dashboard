7626164532a1535e7ce375cb70cbe9fdc9ead773
Lucas Souza <lucasouliveira@gmail.com>
2020-06-20 03:27:37 -0300

    global font smaller + country cases flex

--- web/src/components/CountryCard/index.js
+++ web/src/components/CountryCard/index.js
@@ -2,42 +2,43 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './styles.css'
 import rootpath from '../../services/getRootPath';
+const nf = new Intl.NumberFormat();
 
 export default function CountryCard({ country }) {
-    const nf = new Intl.NumberFormat();
-
     country.iso2 = (country.country === 'MS Zaandam' ? 'US' : country.iso2)
     country.iso2 = (country.country === 'Diamond Princess' ? 'US' : country.iso2)
-    // country.iso2 = (country.iso2 === 'DP' ? 'JP' : country.iso2);
 
     return (
         <div className="col-lg-4">
             <Link to={`${rootpath}${country.iso2}`} className="unstyled-link">
                 <article className="pais-article shadow">
-                    <div className="row justify-content-center">
-                        <div className="col-3">
+                    <div className="row align-items-center">
+                        <aside className="col-3">
                             <div className="row">
-                                <div className="col my-auto">
+                                <figure className="col my-auto">
                                     <div className="bandeira text-center">
                                         {/* <img src={"https://www.countryflags.io/" + country.code + "/flat/64.png"} alt={"Bandeira " + country.title} /> */}
                                         <img className="bandeira-img" src={"https://cdn.u21.io/flags/4x3/" + (country.iso2).toLowerCase() + ".svg"} alt={"Bandeira " + country.country} />
                                     </div>
-                                    <div className="font-weight-bold text-center text-light">{country.country}</div>
+                                    <figcaption className="text-bold font-smaller text-center text-white">{country.country}</figcaption>
+                                </figure>
+                            </div>
+                        </aside>
+                        <div className="col">
+                            <section className="cases">
+                                <div>
+                                    Cases
+                                    <div className="color-cases">{nf.format(country.cases)}</div>
                                 </div>
-                            </div>
-                        </div>
-                        <div className="col my-auto">
-                            <div className="row text-light">
-                                {/* <div className="col font-weight-bold text-center">Pais</div> */}
-                                <div className="col font-weight-bold text-center">Cases</div>
-                                <div className="col font-weight-bold text-center">Deaths</div>
-                                <div className="col font-weight-bold text-center">Recovered</div>
-                            </div>
-                            <div className="row">
-                                <div className="col text-center color-cases">{nf.format(country.cases)}</div>
-                                <div className="col text-center color-death">{nf.format(country.deaths)}</div>
-                                <div className="col text-center color-recovered">{nf.format(country.recovered)}</div>
-                            </div>
+                                <div>
+                                    Deaths
+                                    <div className="color-death">{nf.format(country.deaths)}</div>
+                                </div>
+                                <div>
+                                    Recovered
+                                    <div className="color-recovered">{nf.format(country.recovered)}</div>
+                                </div>
+                            </section>
                         </div>
                     </div>
                 </article>
