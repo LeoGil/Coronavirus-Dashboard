e3eebbda004025509c8b8478bf175b94e970da4e
lucasliet <lucasouliveira@gmail.com>
2020-06-21 21:49:20 -0300

    include-media + header css

--- web/src/components/Header/index.js
+++ web/src/components/Header/index.js
@@ -6,20 +6,16 @@ import rootpath from '../../services/getRootPath';
 import logo from '../../assets/logo.png';
 
 const Header = () => (
-  <div id="main-header" className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 shadow-sm">
-    <h5 className="my-0 mr-md-auto font-weight-normal ">
-      <div className="blobs-container">
-        <img
-          src={logo}
-          alt='Covid Dashboard'
-          className='logo mx-2'
-        />
-        <Link to={rootpath} className="text-white mt-2">
-          DASHBOARD
-        </Link>
-        <div className="blob red"></div>
-      </div>
-    </h5>
+  <div id="main-header">
+    <Link to={rootpath} className="title-brand">
+      <img
+        src={logo}
+        alt='Covid Dashboard'
+        className='logo'
+      />
+            DASHBOARD
+          <div className="blob red"></div>
+    </Link>
     <nav className="my-2 my-md-0 mr-md-3">
       <Link to={rootpath} className="p-2 text-white">Features</Link>
     </nav>
