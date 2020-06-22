bbb5646cd35f86d66e975a4363b1044b9453a36c
lucasliet <lucasouliveira@gmail.com>
2020-06-21 23:09:45 -0300

    🎨️ new GlobalData Blocks

--- web/src/components/GlobalCard/index.js
+++ web/src/components/GlobalCard/index.js
@@ -7,34 +7,34 @@ export default function GlobalData({ globaldata }) {
 
     return (
         <>
-            <h1 className="text-center text-white title-components">Global Data</h1>
-            <article className="global-article shadow">
+            <h1 className="text-center title-components">Global Data</h1>
+            <article className="global-article">
                 <section className="cases">
-                    <div className="global-data-title">
+                    <div className="global-data-title t1">
                         Cases
                         <span className="global-data-numbers color-cases">{nf.format(globaldata.cases)}</span>
                         New cases today
                         <span className="global-data-numbers color-new-case">{nf.format(globaldata.todayCases)}</span>
                     </div>
-                    <div className="global-data-title">
+                    <div className="global-data-title t2">
                         Deaths
                         <span className="global-data-numbers color-death">{nf.format(globaldata.deaths)}</span>
                         New deaths today
                         <span className="global-data-numbers color-new-death">{nf.format(globaldata.todayDeaths)}</span>
                     </div>
-                    <div className="global-data-title">
+                    <div className="global-data-title t3">
                         Recovered
                         <span className="global-data-numbers color-recovered">{nf.format(globaldata.recovered)}</span>
                         New recoveries today
                         <span className="global-data-numbers color-new-recoveries">{nf.format(globaldata.todayRecovered)}</span>
                     </div>
-                    <div className="global-data-title">
+                    <div className="global-data-title t4">
                         Active cases
                         <span className="global-data-numbers color-active">{nf.format(globaldata.active)}</span>
                         Tests
                         <span className="global-data-numbers color-tests">{nf.format(globaldata.tests)}</span>
                     </div>
-                    <div className="global-data-title">
+                    <div className="global-data-title t5">
                         Countries affected
                         <span className="global-data-numbers color-affected">{nf.format(globaldata.affectedCountries)}</span>
                         Criticals
