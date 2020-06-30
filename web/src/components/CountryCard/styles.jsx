1c3f6b88a751b77f985c76143f97d52c08d4ffdb
lucasliet <lucasouliveira@gmail.com>
2020-06-30 01:41:48 -0300

    ⬆ fix CountryCard top animation

--- web/src/components/CountryCard/styles.jsx
+++ web/src/components/CountryCard/styles.jsx
@@ -5,25 +5,27 @@ const CountryCardStyles = styled.div`
   .pais-article {
     margin: 20px auto 10px;
     padding: 20px;
-    border-radius: 0.5rem;
-    background: ${colors.bgActive};
-    font-weight: 800;
+
     font-size: 18px;
+    font-weight: 800;
+    background: ${colors.bgActive};
+    border-radius: 0.5rem;
     border: 3px solid ${colors.bgActive};
+    box-shadow: 0.2rem 1rem 0.65rem rgba(0, 0, 0, 0.342);
+
+    transition: all ease-in-out 0.09s;
     &:hover {
       filter: brightness(1.2);
-      transition: all 0.2s;
-      position: relative;
-      top: -10px;
+      transform: translateY(-10px);
       border: 3px solid ${colors.hoverBorder};
     }
-    box-shadow: 0.2rem 1rem 0.65rem rgba(0, 0, 0, 0.342);
     .flag {
-      margin-right: 55px;
       display: grid;
       grid-template-columns: 1fr 5fr;
-      font-size: x-large;
       align-items: center;
+
+      margin-right: 55px;
+      font-size: x-large;
       img {
         height: 48px;
         border-radius: 20%;
@@ -34,6 +36,7 @@ const CountryCardStyles = styled.div`
     .cases {
       display: flex;
       flex-wrap: wrap;
+
       min-height: 67px;
       div {
         flex: 1 1 auto;
@@ -43,8 +46,8 @@ const CountryCardStyles = styled.div`
     }
 
     .new-today {
-      font-size: 12px;
       margin-top: -5px;
+      font-size: 12px;
     }
   }
 `;
